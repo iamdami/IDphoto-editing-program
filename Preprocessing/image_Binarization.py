e4cc0d1e16eb9d12eedbd97cf87eeaaf85e0f885
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-04-15 09:59:18 +0900

    Update image_Binarization.py

--- Preprocessing/image_Binarization.py
+++ Preprocessing/image_Binarization.py
@@ -16,3 +16,14 @@ image_binarized = cv2.adaptiveThreshold(image_grey, max_output_value,
                                          
 plt.imshow(image_binarized, cmap="gray"), plt.axis("off")
 plt.show()
+
+
+# cv2.ADAPTIVE_THRESH_MEAN_C 적용
+image_mean_threshold = cv2.adaptiveThreshold(image_grey,
+                                             max_output_value,
+                                             cv2.ADAPTIVE_THRESH_MEAN_C,
+                                             cv2.THRESH_BINARY,
+                                             neighborhood_size,
+                                             subtract_from_mean)
+plt.imshow(image_mean_threshold, cmap="gray"), plt.axis("off")
+plt.show()
